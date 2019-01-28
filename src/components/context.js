1352d90d3d9e368dbb298ad64a05fa9a8d968974
shrey  khanna <shreykhanna@shreys-MacBook-Pro.local>
2019-01-28 21:15:22 +1000

    Added product Increment/Decrement functionality

--- src/components/context.js
+++ src/components/context.js
@@ -61,12 +61,31 @@ class ProductProvider extends React.Component{
     },()=>{console.log("ERROR")})
   };
   increment=(id)=>{
-    console.log("Inside increment method")
-  }
+    let tempCart=[...this.state.cart]
+    const selectedProduct=tempCart.find(item=>item.id===id)
+    const index=tempCart.indexOf(selectedProduct)
+    let product=tempCart[index]
+    product.count=product.count+1
+    product.total=product.price*product.count
+      this.setState(()=>{
+        return{cart:[...tempCart]}
+    },()=>{this.addTotal()})}
   decrement=(id)=>{
     let tempCart=[...this.state.cart]
-    tempCart=tempCart.filter(item => item.id!==id)
-  }
+    const selectedProduct=tempCart.find(item => item.id===id)
+    const index=tempCart.indexOf(selectedProduct)
+    let product=tempCart[index]
+    product.count=product.count-1
+    if(product.count==0){this.removeItem(id)}
+    else{
+    product.total=product.price*product.count
+    this.setState(()=>{
+      return{
+        cart:[...tempCart]
+      }
+    },()=>{this.addTotal()})
+}
+    }
   removeItem=(id)=>{
     let tempProducts=[...this.state.products]
     let tempCart=[...this.state.cart]
