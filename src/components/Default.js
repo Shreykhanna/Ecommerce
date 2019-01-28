1915efa566f8d4c2771761de70061954b57394d3
shrey  khanna <shreykhanna@shreys-MacBook-Pro.local>
2019-01-29 04:56:58 +1000

    Added page not found route.

--- src/components/Default.js
+++ src/components/Default.js
@@ -4,8 +4,20 @@ export default class Default extends React.Component{
 
 render()
 {
+  console.log(this.props)
   return(
-    <h3>Inside default page</h3>
+    <div className="container">
+    <div className="row">
+      <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+        <h1 className="display-3">404</h1>
+        <h1>error</h1>
+        <h2>page not found</h2>
+        <h3>Requested url not found</h3>
+        <span className="text-danger">{this.props.location.pathname}</span>{""}
+        was not found
+      </div>
+    </div>
+    </div>
   )
 }
 
