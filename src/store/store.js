e5a3bcc19b3e3be35cd4f30d05470ec2a9dc6ffd
ShreyKhanna <khannashrey07@gmail.com>
2020-01-08 00:20:19 +1000

    Added redux-persist for persistence

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,35 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from '../reducers/root-reducer';
 
+function saveToLocalStorage(state)
+{
+  try{
+    const serializedState=JSON.stringify(state)
+    localStorage.setItem('state',serializedState)
+
+  }catch(e){
+    console.log("Error : ",e);
+  }
+}
+function loadFromLocalStorage()
+{
+  try{
+    const serializedState=localStorage.getItem('state');
+    if(serializedState===null) return undefined
+    return JSON.parse(serializedState)
+
+  }catch(e){
+    console.log(e);
+  }
+}
+const persistedState=loadFromLocalStorage();
 const initialState = {};
 const store = createStore(
   rootReducer,
-  initialState,
+  persistedState,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+store.subscribe(()=>saveToLocalStorage(store.getState()))
 
 export default store;
